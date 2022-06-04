89ab1cf424e0146b9df3d9ec6c4c84a3ddf0cbc3
JunqiaoYe <junqiao.ye@qq.com>
2022-06-04 20:29:23 +0800

    first commit

---
+++ app/src/main/java/com/example/androidhomework/AccountBookSetting.java
@@ -0,0 +1,19 @@
+package com.example.androidhomework;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountBookSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_book_setting);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+}
