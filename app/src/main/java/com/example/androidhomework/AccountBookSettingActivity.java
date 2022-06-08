5074e4ed202523decaaa8cf90683cdf65b6c06c3
JunqiaoYe <yejunqiao1998@qq.com>
2022-06-08 16:24:34 +0800

    second commit

--- app/src/main/java/com/example/androidhomework/AccountBookSettingActivity.java
+++ app/src/main/java/com/example/androidhomework/AccountBookSettingActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AccountBookSetting extends AppCompatActivity {
+public class AccountBookSettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
