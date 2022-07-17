85b133904ea924f63ee28d75b5de106f22429e1d
JunqiaoYe <yejunqiao1998@qq.com>
2022-07-17 22:11:26 +0800

    WebView - first commit

--- app/src/main/java/com/example/homework/MainActivity.java
+++ app/src/main/java/com/example/homework/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.homework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebViewClient;
 
 import com.example.homework.databinding.ActivityMainBinding;
 
@@ -13,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.webView.getSettings().setJavaScriptEnabled(true);
+        binding.webView.setWebViewClient(new WebViewClient());
+        binding.webView.loadUrl("https://www.baidu.com");
     }
 }
