4bd8705a638d268cada04d87b34cde1589b302f2
JunqiaoYe <yejunqiao1998@qq.com>
2022-07-17 22:20:58 +0800

    HttpURLConnection - first commit

--- app/src/main/java/com/example/homework/MainActivity.java
+++ app/src/main/java/com/example/homework/MainActivity.java
@@ -6,12 +6,61 @@ import android.os.Bundle;
 
 import com.example.homework.databinding.ActivityMainBinding;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.sendRequestButton.setOnClickListener(view -> sendRequestWithHttpURLConnection());
     }
+
+    private void sendRequestWithHttpURLConnection() {
+        new Thread(() -> {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+            try {
+                StringBuilder response = new StringBuilder();
+                URL url = new URL("https://www.baidu.com");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setConnectTimeout(8000);
+                connection.setReadTimeout(8000);
+                InputStream input = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(input));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                showResponse(response.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+        }).start();
+    }
+
+    private void showResponse(final String response) {
+        runOnUiThread(() -> binding.responseText.setText(response));
+    }
+
 }
